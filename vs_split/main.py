deb09a53ee9d805c5752254693b6c82e44e05889
Lj Miranda <ljvmiranda@gmail.com>
2022-07-09 22:31:49 +0800

    Run isort on the file

--- vs_split/main.py
+++ vs_split/main.py
@@ -1,8 +1,9 @@
 from typing import Iterable
 
 from spacy.tokens import Doc
-from vs_split.splitters import splitters as splitter_catalogue
 from wasabi import msg
+
+from vs_split.splitters import splitters as splitter_catalogue
 
 
 def train_test_split(X: Iterable, y: Iterable, split_id: str, **attrs):
